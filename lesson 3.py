83b850ed875c06f7e752198bc2641df49856b92f
tazo xhachidze <166508015+sgfhr4364JIW4UQ5@users.noreply.github.com>
2024-05-21 16:59:37 +0400

    Add files via upload

---
+++ lesson 3.py
@@ -0,0 +1,18 @@
+print(15+61)
+print(29+42)
+print(79+11)
+print(17+57)
+print(42-28)
+print(91-67)
+print(70-43)
+print(90-25)
+print(15*93)
+print(27*42)
+print(50*58)
+print(41*36)
+print(67/11)
+print(99/41)
+print(45/67)
+print(81/62)
+stringi aris ori zemot atanili ori wertili
+
