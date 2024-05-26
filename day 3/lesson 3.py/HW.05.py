74b86164a1f5337a9ca5e953214f5bbaf0fc0fda
tazo xhachidze <166508015+sgfhr4364JIW4UQ5@users.noreply.github.com>
2024-05-26 19:31:09 +0400

    Add files via upload

---
+++ day 3/lesson 3.py/HW.05.py
@@ -0,0 +1,11 @@
+print(12+61)
+#CHVEN 12 DAVUMATET 61 DA MIVIGET 73
+
+print(42-23)
+#42 gamovaklet 23 ro migvego 19
+
+print(82*51)
+#82-jer 51 aris 4182
+
+print(61/52)
+#61 gavyet 51 da miviget 1.1730769230769231
